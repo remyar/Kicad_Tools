74a3390649e26d37c89f034b17e0203887844426
ARRT <remy.arnaudin@wanadoo.fr>
2019-07-23 20:04:33 +0200

    add settings

--- src/actions/settings.js
+++ src/actions/settings.js
@@ -1,12 +1,15 @@
-import Api from '../api';
-
-
+import Store from 'electron-store';
+const store = new Store();
 
 function get( key ){
- 
+    return store.get(key);
+}
+
+function set(key , val){
+    store.set(key , val);
 }
 
 export default {
-
+    set,
     get,
 }
