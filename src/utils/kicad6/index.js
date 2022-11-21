5d2a10780e696f49a637dce11935ea79d514a1aa
Rémy <remy.arnaudin@sauermanngroup.com>
2022-11-21 10:39:24 +0100

    fix export footprint

--- src/utils/kicad6/index.js
+++ src/utils/kicad6/index.js
@@ -2,6 +2,7 @@ import { ExporterSymbolKicad } from "./export_kicad_symbol";
 import { ExporterFootprintKicad } from "./export_kicad_footprint";
 import { Exporter3dModelKicad } from "./export_kicad_3d_model";
 import sExpression from 's-expression';
+import _ from "lodash";
 
 async function getSymbol(component, librarieName) {
     return new Promise(async (resolve, reject) => {
@@ -9,7 +10,7 @@ async function getSymbol(component, librarieName) {
             if ( component.description ){
                 component.symbol.info.description = component.description;
             }
-            let exporter = new ExporterSymbolKicad(component.symbol);
+            let exporter = new ExporterSymbolKicad(_.cloneDeep(component.symbol));
             let kicad_symbol_lib = exporter.export(false, librarieName);
 
             resolve(
@@ -24,7 +25,8 @@ async function getSymbol(component, librarieName) {
 async function getFootprint(component, librarieName) {
     return new Promise((resolve, reject) => {
         try {
-            let exporter = new ExporterFootprintKicad(component.footprint);
+            
+            let exporter = new ExporterFootprintKicad( _.cloneDeep(component.footprint));
             let kicad_footprint_lib = exporter.export(false, librarieName);
 
             resolve(
@@ -39,7 +41,7 @@ async function getFootprint(component, librarieName) {
 async function get3DModel(component , librarieName ){
     return new Promise(async (resolve, reject) => {
         try {
-            let exporter = new Exporter3dModelKicad(component.model3D);
+            let exporter = new Exporter3dModelKicad(_.cloneDeep(component.model3D));
             let kicad_3d_model = exporter.export();
             resolve(kicad_3d_model);
         } catch (err) {
